7c82e80ea9460927f259dc78a2a813dc3f8fb692
RosellaHung <twhung@uci.edu>
2024-08-24 21:38:41 -0700

    create new AP object if startwith AP

--- runner.py
+++ runner.py
@@ -17,17 +17,12 @@ from AP import APclass
 # #                        ["CLIENTNAME", "x", "y", "standard", "speed", "supports_11k",
 # #                         "supports_11v", "supports_11r", "minimal_rssi"])
 # # Move_nt = namedtuple("Move", ["CLIENT", "x", "y"])
+all_ap = []
 
 def run(file: str):
     with open(file, "r") as file:
         content = file.readlines()
         for line in content:
-            # line_lst = line.strip().split()
-            # print(line_lst)
-            # if line_lst == []:
-            #     pass
-            # elif not line_lst[0] in ("AP", "CLIENT", "MOVE"):
-            #     raise ValueError(f"Invalid line format: {line.strip()}")
             process_lines(line)
 
 def process_lines(line):
@@ -36,6 +31,11 @@ def process_lines(line):
         pass
     elif not line_lst[0] in ("AP", "CLIENT", "MOVE"):
         raise ValueError(f"Invalid line format: {line.strip()}")
+    elif line_lst[0] == "AP":
+        Ap = APclass(*line_lst[1:])
+        all_ap.append(Ap)
+
+
 
 
 
