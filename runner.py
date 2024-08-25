8f93489d10b1ca1e743d33a26e01402ff95ab3c7
RosellaHung <twhung@uci.edu>
2024-08-24 21:21:14 -0700

    process line in a different function, and decided to not use namedtuple

--- runner.py
+++ runner.py
@@ -1,21 +1,22 @@
 from collections import namedtuple
-AP_dict = {"APNAME": "",
-            "x": 0,
-            "y": 0,
-            "channel": 0,
-            "powerlevel": 0,
-            "frequency": "",
-            "standard": "",
-            "supports_11k": False,
-            "supports_11v": False,
-            "supports_11r": False,
-            "coverage_radius": 0,
-            "device_limit": 0,
-            "minimal_rssi": None}
-Client_nt = namedtuple("Client",
-                       ["CLIENTNAME", "x", "y", "standard", "speed", "supports_11k",
-                        "supports_11v", "supports_11r", "minimal_rssi"])
-Move_nt = namedtuple("Move", ["CLIENT", "x", "y"])
+from AP import APclass
+# AP_dict = {"APNAME": "",
+# #             "x": 0,
+# #             "y": 0,
+# #             "channel": 0,
+# #             "powerlevel": 0,
+# #             "frequency": "",
+# #             "standard": "",
+# #             "supports_11k": False,
+# #             "supports_11v": False,
+# #             "supports_11r": False,
+# #             "coverage_radius": 0,
+# #             "device_limit": 0,
+# #             "minimal_rssi": None}
+# # Client_nt = namedtuple("Client",
+# #                        ["CLIENTNAME", "x", "y", "standard", "speed", "supports_11k",
+# #                         "supports_11v", "supports_11r", "minimal_rssi"])
+# # Move_nt = namedtuple("Move", ["CLIENT", "x", "y"])
 
 def run(file: str):
     with open(file, "r") as file:
