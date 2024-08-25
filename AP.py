6a542572c3caf28eca655e2b0874fbef0e8b52a5
RosellaHung <twhung@uci.edu>
2024-08-24 21:21:38 -0700

    made a skeleton of the AP class

---
+++ AP.py
@@ -0,0 +1,13 @@
+import math
+class APclass:
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def roam(self):
+        pass
