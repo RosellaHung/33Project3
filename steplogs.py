5be308749df9641034d43417722a4050db9c2634
RosellaHung <twhung@uci.edu>
2024-08-26 20:20:25 -0700

    created an inheritance class so it tracks specific entity names and generate and write binary file

--- steplogs.py
+++ steplogs.py
@@ -1,3 +1,4 @@
+import pickle
 class steplogger:
     def __init__(self):
         self.step_lst = []
@@ -5,8 +6,20 @@ class steplogger:
 
     def add_new_log(self, new_step):
         self.step_count += 1
-        self.step_lst.append(f"step {self.step_count}: {new_step}")
+        self.step_lst.append(f"Step {self.step_count}: {new_step}")
 
     def __str__(self):
-        for step in self.step_lst:
-            print(step)
+        return "\n".join(self.step_lst)
+
+
+class specific_logger(steplogger):
+    def __init__(self, entity_name):
+        super().__init__()
+        self.entity_name = entity_name
+
+    def write_new(self, event):
+        self.add_new_log(event)
+
+    def __call__(self, filename):
+        with open(f"{self.entity_name}_log.pkl", "wb") as binary_file:
+            pickle.dump(self.step_lst, binary_file)
