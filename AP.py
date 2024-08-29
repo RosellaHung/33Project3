7868d4128f04183e8fa91d5d5caa9b4a049127ab
RosellaHung <twhung@uci.edu>
2024-08-28 19:37:44 -0700

    Connection between ap and client

--- AP.py
+++ AP.py
@@ -1,8 +1,9 @@
 import math
-from steplogs import specific_logger
+from steplogs import specific_logger, steplogger
 class APclass:
-    def __init__(self, APNAME, x, y, channel, powerlevel, frequency, standard, supports_11k, supports_11v,
+    def __init__(self, steplogger, ac,  APNAME, x, y, channel, powerlevel, frequency, standard, supports_11k, supports_11v,
                 supports_11r, coverage_radius, device_limit, minimal_rssi=None):
+        self.steplogger = steplogger
         self._apname = APNAME
         self.x = x
         self.y = y
@@ -32,9 +33,14 @@ class APclass:
         rssi = self.calculate_rssi(client)
         if self.check_availablity() and not rssi is None and (self._minimal_rssi is None or rssi > self._minimal_rssi):
             self.connecting_clients.append(client)
-            self.steplogger.step_lst.append(f"CLIENT CONNECT TO {self._apname} WITH SIGNAL STRENGTH 62")
-        else:
-            self.steplogger.step_lst.append(f"{client.client_name} TRIED {self._apname} BUT WAS DENIED")
+            client.connected_ap = self
+            self.steplogger.add_new_log(f"{client._client_name} CONNECT LOCATION {client.x} {client.y} {client._standard} {client._frequency} {client._supports11k} {client._supports11v} {client._supports11r}")  # Yet to change
+            self.logger.add_new_log(f"{client._client_name} CONNECT LOCATION {client.x} {client.y} {client._standard} {client._frequency} {client._supports11k} {client._supports11v} {client._supports11r}") # Yet to change
+            return True, rssi
+        # else:
+        #     self.steplogger.add_new_log(f"{client._client_name} TRIED {self._apname} BUT WAS DENIED")
+        #     self.logger.add_new_log(f"{client._client_name} TRIED {self._apname} BUT WAS DENIED")
+        return False
 
     def disconnect(self): # when client is out of range
         pass
@@ -42,5 +48,5 @@ class APclass:
     def roam(self):
         pass
 
-    def __call__(self):
-        return self.logger.generate(self._apname)
+    def __call__(self, filename):
+        return self.logger.generate(filename)
--- Client.py
+++ Client.py
@@ -1,14 +1,39 @@
+from steplogs import specific_logger
+from AC import ACclass
 class Clientclass:
-    def __init__(self, x, y, standard, speed, supports_11k, supports_11v, supports_11r, minimal_rssi):
+    def __init__(self, steplogger, ac, client_name, x, y, standard, frequency, supports_11k, supports_11v, supports_11r, minimal_rssi):
+        self.steplogger = steplogger
+        self.ac = ac
+        self._client_name = client_name
         self.x = x
         self.y = y
-        self.standard = standard
-        self.speed = speed
-        self.supports11k = supports_11k
-        self.supports11v = supports_11v
-        self.supports11r = supports_11r
-        self.minimal_rssi = minimal_rssi
+        self._standard = standard
+        self._frequency = frequency
+        self._supports11k = supports_11k
+        self._supports11v = supports_11v
+        self._supports11r = supports_11r
+        self._minimal_rssi = minimal_rssi
+        self.logger = specific_logger(client_name)
+        self.connected_ap = None
 
     def move(self, x_cor, y_cor):
         self.x = x_cor
         self.y = y_cor
+
+    def connect_to_ap(self):
+        best_ap = self.ac.evaluate_best_ap(self)
+        for ap in best_ap:
+            result = ap.connect(self)
+            if result[0]:
+                self.steplogger.add_new_log(f"CLIENT CONNECT TO {ap._apname} WITH SIGNAL STRENGTH {result[1]}")
+                self.logger.add_new_log(f"CLIENT CONNECT TO {ap._apname} WITH SIGNAL STRENGTH {result[1]}")
+
+    def disconnect(self):
+        pass
+
+
+    def roam(self):
+        pass
+
+    def __call__(self):
+        return self.logger.generate(self._apname)
