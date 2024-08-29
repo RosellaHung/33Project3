4d3c9d17538d045b739312b985572d5f61e23b8c
RosellaHung <twhung@uci.edu>
2024-08-28 19:40:04 -0700

    updated specific_logger class

--- steplogs.py
+++ steplogs.py
@@ -9,17 +9,23 @@ class steplogger:
         self.step_lst.append(f"Step {self.step_count}: {new_step}")
 
     def __str__(self):
-        return "\n".join(self.step_lst)
-
+        for step in self.step_lst:
+            print(step)
 
 class specific_logger(steplogger):
     def __init__(self, entity_name):
         super().__init__()
         self.entity_name = entity_name
 
-    def write_new(self, event):
-        self.add_new_log(event)
+    # def add_new_log(self, new_event):
+    #     self.step_count += 1
+    #     self.step_lst.append(f"Step {self.step_count}: {new_event}")
 
-    def __call__(self, filename):
+    def generate(self, filename):
         with open(f"{self.entity_name}_log.bin", "wb") as binary_file:
             pickle.dump(self.step_lst, binary_file)
+
+#do we only generate binary file when device is called?
+
+
+
