b23c9665c9c3a8b615082db4fed1b6c65511257e
RosellaHung <twhung@uci.edu>
2024-09-02 04:34:43 +0800

    log all steps to local logger and ac logger

--- AP.py
+++ AP.py
@@ -1,52 +1,60 @@
 import math
-from steplogs import specific_logger, steplogger
+from steplogs import specific_logger
 class APclass:
-    def __init__(self, steplogger, ac, APNAME, x, y, channel, powerlevel, frequency, standard, supports_11k, supports_11v,
+    def __init__(self, ac, APNAME, x, y, channel, powerlevel, frequency, standard, supports_11k, supports_11v,
                 supports_11r, coverage_radius, device_limit, minimal_rssi=None):
-        self.steplogger = steplogger
+        self.ac = ac
         self._apname = APNAME
-        self.x = x
-        self.y = y
+        self.x = int(x)
+        self.y = int(y)
         self.channel = channel
-        self._powerlevel = powerlevel
-        self._frequency = frequency
+        self._powerlevel = int(powerlevel)
+        if len(frequency) > 1:
+            self._frequency = frequency.split('/')
+        elif len(frequency) == 1:
+            self._frequency = [frequency]
         self._standard = standard
         self._supports_11k = supports_11k
         self._supports_11v = supports_11v
         self._supports_11r = supports_11r
-        self._coverage_radius = coverage_radius
-        self._device_limit = device_limit
+        self._coverage_radius = int(coverage_radius)
+        self._device_limit = int(device_limit)
         self._minimal_rssi = minimal_rssi
         self.connecting_clients = []
         self.logger = specific_logger(self._apname)
 
     def calculate_rssi(self, client):
-        distance = math.sqrt((self.x - client.x) ** 2 + (self.y - client.y) ** 2)
+        result = None
+        for cghz in client._frequency:
+            for ghz in self._frequency:
+                if ghz == cghz:
+                    result = float(ghz) * 1000
+        distance = math.sqrt((self.x - int(client.x)) ** 2 + (self.y - int(client.y)) ** 2)
         if distance > self._coverage_radius:
             return None
-        return self._powerlevel - 20 * math.log10(distance) - 20 * math.log10(self._frequency) - 32.44
+        return self._powerlevel - 20 * math.log10(distance) - 20 * math.log10(result) - 32.44
 
     def check_availablity(self):
         return len(self.connecting_clients)  < self._device_limit
 
     def connect(self, client):
-        rssi = self.calculate_rssi(client)
-        if self.check_availablity() and not rssi is None and (self._minimal_rssi is None or rssi > self._minimal_rssi):
+        rssi = abs(self.calculate_rssi(client))
+        if self.check_availablity() and not rssi is None and (self._minimal_rssi is None or rssi > float(self._minimal_rssi)):
             self.connecting_clients.append(client)
             client.connected_ap = self
-            self.steplogger.add_new_log(f"{client._client_name} CONNECT LOCATION {client.x} {client.y} {client._standard} {client._frequency} {client._supports11k} {client._supports11v} {client._supports11r}")  # Yet to change
-            self.logger.add_new_log(f"{client._client_name} CONNECT LOCATION {client.x} {client.y} {client._standard} {client._frequency} {client._supports11k} {client._supports11v} {client._supports11r}") # Yet to change
+            self.logger.add_new_log(f"{client._client_name} CONNECT LOCATION {client.x} {client.y} {client._standard} {"/".join(client._frequency)} {client._supports11k} {client._supports11v} {client._supports11r}") # Yet to change
+            self.ac.logger.add_new_log(f"{client._client_name} CONNECT LOCATION {client.x} {client.y} {client._standard} {"/".join(client._frequency)} {client._supports11k} {client._supports11v} {client._supports11r}")  # Yet to change
             return True, rssi
-        return False
+        return False, rssi
 
     def disconnect(self, client): # when client is out of range
         self.connecting_clients = [c for c in self.connecting_clients if not client]
-        self.steplogger.add_new_log(f"{client._client_name} DISCONNECT AT LOCATION {client.x} {client.y}")
         self.logger.add_new_log(f"{client._client_name} DISCONNECT AT LOCATION {client.x} {client.y}")
+        self.ac.logger.add_new_log(f"{client._client_name} DISCONNECT AT LOCATION {client.x} {client.y}")
 
     def roam(self):
         pass
 
 
-    def __call__(self, filename):
-        return self.logger.generate(filename)
+    def __call__(self):
+        return self.logger.generate(self._apname)
--- Client.py
+++ Client.py
@@ -1,49 +1,52 @@
 from steplogs import specific_logger
 from AC import ACclass
 class Clientclass:
-    def __init__(self, steplogger, ac, client_name, x, y, standard, frequency, supports_11k, supports_11v, supports_11r, minimal_rssi):
-        self.steplogger = steplogger
+    def __init__(self, ac, client_name, x, y, standard, frequency, supports_11k, supports_11v, supports_11r, minimal_rssi):
         self.ac = ac
+        self.client = self
         self._client_name = client_name
         self.x = x
         self.y = y
         self._standard = standard
-        self._frequency = frequency
+        if len(frequency) > 1:
+            self._frequency = frequency.split('/')
+        elif len(frequency) == 1:
+            self._frequency = [frequency]
         self._supports11k = supports_11k
         self._supports11v = supports_11v
         self._supports11r = supports_11r
-        self._minimal_rssi = minimal_rssi
+        self._minimal_rssi = float(minimal_rssi)
         self.logger = specific_logger(client_name)
         self.connected_ap = None
 
     def move(self, x_cor, y_cor):
         self.x = x_cor
         self.y = y_cor
-        best_ap_result = ACclass.evaluate_best_ap(self)
-        if best_ap_result is "Out Of Range":
+        best_ap_result = self.ac.evaluate_best_ap(self)
+        if best_ap_result == "Out Of Range":
             self.disconnect()
         else:
             self.roam(best_ap_result)
 
 
     def connect_to_ap(self):
-        best_ap = self.ac.evaluate_best_ap(self)
-        for ap in best_ap:
-            result = ap.connect(self)
-            if result[0]:
-                self.steplogger.add_new_log(f"CLIENT CONNECT TO {ap._apname} WITH SIGNAL STRENGTH {result[1]}")
-                self.logger.add_new_log(f"CLIENT CONNECT TO {ap._apname} WITH SIGNAL STRENGTH {result[1]}")
+        best_ap = self.ac.evaluate_best_ap(self.client)
+        result = best_ap.connect(self.client)
+        if result[0] is True:
+            self.logger.add_new_log(f"CLIENT CONNECT TO {best_ap._apname} WITH SIGNAL STRENGTH {result[1]:.2f}")
+            self.ac.logger.add_new_log(f"{self._client_name} CONNECT TO {best_ap._apname} WITH SIGNAL STRENGTH {result[1]:.2f}")
 
     def disconnect(self):
         strength = self.connected_ap.calculate_rssi(self)
         self.connected_ap.disconnect(self)
         self.connected_ap = None
-        self.steplogger.add_new_log( f"CLIENT DISCONNECT FROM {self.connected_ap._apname} WITH SIGNAL STRENGTH {abs(strength)}")
-        self.logger.add_new_log(f"CLIENT DISCONNECT FROM {self.connected_ap._apname} WITH SIGNAL STRENGTH {abs(strength)}")
+        self.logger.add_new_log(f"CLIENT DISCONNECT FROM {self.connected_ap._apname} WITH SIGNAL STRENGTH {abs(strength):.2f}")
+        self.ac.logger.add_new_log(f"{self._client_name} DISCONNECT FROM {self.connected_ap._apname} WITH SIGNAL STRENGTH {abs(strength):.2f}")
 
 
     def roam(self, ap):
-        pass
+        best_ap = self.ac.evaluate_best_ap(self.client)
+        result = best_ap.roam(self.client)
 
     def __call__(self):
         return self.logger.generate(self._client_name)
