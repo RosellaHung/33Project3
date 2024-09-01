974f6e91676ba5bdfe19a6e1a22039872c9e94c6
RosellaHung <twhung@uci.edu>
2024-09-02 04:35:52 +0800

    fixed __str__

--- steplogs.py
+++ steplogs.py
@@ -1,31 +1,19 @@
 import pickle
-class steplogger:
-    def __init__(self):
+class specific_logger():
+    def __init__(self, entity_name):
         self.step_lst = []
         self.step_count = 0
+        self.entity_name = entity_name
 
     def add_new_log(self, new_step):
         self.step_count += 1
         self.step_lst.append(f"Step {self.step_count}: {new_step}")
 
-    def __str__(self):
-        for step in self.step_lst:
-            print(step)
-
-class specific_logger(steplogger):
-    def __init__(self, entity_name):
-        super().__init__()
-        self.entity_name = entity_name
-
-    # def add_new_log(self, new_event):
-    #     self.step_count += 1
-    #     self.step_lst.append(f"Step {self.step_count}: {new_event}")
-
     def generate(self, filename):
         with open(f"{self.entity_name}_log.bin", "wb") as binary_file:
             pickle.dump(self.step_lst, binary_file)
-
-#do we only generate binary file when device is called?
-
+    def __str__(self):
+        result = "\n".join(self.step_lst)
+        return result
 
 
